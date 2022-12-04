adb42cc3e70ea22360671f2dfd50ab4a13df00a3
elsa-kim <ksh96611@naver.com>
2022-12-04 18:08:19 +0900

    Update mongoConnect.js

--- controllers/mongoConnect.js
+++ controllers/mongoConnect.js
@@ -1,4 +1,4 @@
-// mongodb 연결용
+// mongodb 연결용 => mongo 가서 복사해오는 거
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = "mongodb+srv://sohyun:1236@cluster0.k9iy3rj.mongodb.net/?retryWrites=true&w=majority";
